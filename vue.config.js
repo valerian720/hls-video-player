d9fb1cf46c40af8d6612905502370ed845961126
Валериан Воротников <valerian7202@gmail.com>
2023-05-24 19:09:57 +0300

    auto fix on save + deploy prep

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/hls-video-player/" : "/",
   transpileDependencies: true,
+  // auto fix on save
+  chainWebpack: (config) => {
+    config.plugin("eslint").tap((args) => {
+      args[0].fix = true;
+      return args;
+    });
+  },
 });
